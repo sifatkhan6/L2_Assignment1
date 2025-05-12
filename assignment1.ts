e1da40553249ed552ff7100e2c05af285329a9aa
sifatkhan6 <sifatkhann229@gmail.com>
2025-05-13 02:26:59 +0800

    second task done

--- assignment1.ts
+++ assignment1.ts
@@ -7,4 +7,14 @@ function formatString(input: string, toUpper?: boolean): string {
     }
 }
 
-console.log(formatString('HEllo', true));
+function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
+    return items.filter(item => item.rating >= 4)
+}
+
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 }
+];
+
+console.log(filterByRating(books));
