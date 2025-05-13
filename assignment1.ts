7f8622b566a9d3d151ec26840e3455ad851e4997
sifatkhan6 <sifatkhann229@gmail.com>
2025-05-13 23:57:02 +0800

    fourth task done

--- assignment1.ts
+++ assignment1.ts
@@ -18,6 +18,34 @@ function concatenateArrays<T>(...arrays: T[][]): T[] {
 }
 
 
+class Vehicle {
+  private make: string;
+  private year: number;
+
+  constructor(make: string, year: number) {
+    this.make = make;
+    this.year = year;
+  }
+
+  public getInfo(): string {
+    return `Make: ${this.make}, Year: ${this.year}`;
+  }
+}
+
+class Car extends Vehicle {
+  private model: string;
+
+  constructor(make: string, year: number, model: string) {
+    super(make, year);
+    this.model = model;
+  }
+
+  public getModel(): string {
+    return `Model: ${this.model}`;
+  }
+}
+
+
 function processValue(value: string | number): number {
     if (typeof value === 'string') {
         return value.length;
